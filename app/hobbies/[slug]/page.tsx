1d0362eb04121835df91028f0426d43588890d28
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2023-11-20 19:12:16 +0100

    deploy

--- app/hobbies/[slug]/page.tsx
+++ app/hobbies/[slug]/page.tsx
@@ -1,22 +1,22 @@
-import { PortableText } from '@portabletext/react'
-import { getPost } from '../../../hobbyisthub-backend/sanity-utils'
-import ProductCard from '../../components/ProductCard'
-import { Product } from '../../types/Product'
+import {PortableText} from "@portabletext/react"
+import {getPost} from "../../../hobbyisthub-backend/sanity-utils"
+import ProductCard from "../../components/ProductCard"
+import {Product} from "../../types/Product"
 
-export default async function Post({ params }: any) {
+export default async function Post({params}: any) {
   const post = await getPost(params.slug)
   console.log(post.content[0].products)
 
   return (
-    <div className="mx-auto max-w-screen-lg p-4 bg-white rounded text-black text-justify">
-      <div className="mx-auto p-8 md:px-36 flex flex-col gap-4">
-        <h1 className="text-2xl text-center">{post.title}</h1>
+    <div className='mx-auto max-w-screen-lg p-4 bg-white rounded text-black text-justify'>
+      <div className='mx-auto p-8 md:px-36 flex flex-col gap-4'>
+        <h1 className='text-2xl text-center'>{post.title}</h1>
         <p>{post.description}</p>
         <div>
           {post.content.map((section: any) => (
-            <div className="my-4">
+            <div className='my-4' key={post.id}>
               <PortableText value={section.details} />
-              <div className="flex flex-col gap-2 justify-between mt-4">
+              <div className='flex flex-col gap-2 justify-between mt-4'>
                 {section.products.map((product: Product) => (
                   <ProductCard
                     title={product.title}
--- hobbyisthub-backend/sanity.config.ts
+++ hobbyisthub-backend/sanity.config.ts
@@ -1,14 +1,14 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
+
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'hobbyisthub-backend',
-  projectId: 'd7lyxbp2',
+  projectId: process.env.SANITY_PROJECT_ID as string,
   dataset: 'production',
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(),],
   schema: {
     types: schemaTypes,
   },
