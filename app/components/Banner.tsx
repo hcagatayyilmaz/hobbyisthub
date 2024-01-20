e86acc040e4e475cadba0d95fdb6659c9c9bed2a
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-01-20 14:40:39 +0100

    zort

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,7 +6,7 @@ function Banner() {
   return (
     <div className='bg-[url(/background.png)] bg-center flex justify-center items-center bg-black/60 text-black h-[45vh] w-full border-4 md:border-slate-300  rounded-xl '>
       <div
-        className={`text-white bg-black/50 w-full p-8 flex justify-center rounded text-center text-base md:text-4xl mb-24 ${relaway.className}`}
+        className={`text-white bg-black/50 w-full p-8 flex justify-center rounded text-center text-2xl md:text-4xl mb-24 ${relaway.className}`}
       >
         The gateway to hobby excellence. Unleash your hobbyist spirit, fully
         equipped!
